1f816bb441a40b9bee9e38d5aeb476f8305e9ffe
Toiongo <45739624+Toiongo@users.noreply.github.com>
2020-11-05 08:43:21 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const decoder={r:"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN0PQRSTUVWXYZO123456789+/=",vkId:0,getRealLink:function(a){var b=String.fromCharCode;if(~a.indexOf("audio_api_unavailable")){var c=a.split("?extra=")[1].split("#"),d=""===c[1]?"":this.a(c[1]);if(c=this.a(c[0]),"string"!=typeof d||!c)return a;for(var f,g,h=(d=d?d.split(b(9)):[]).length;h--;){if(f=(g=d[h].split(b(11))).splice(0,1,c)[0],!this.decodeUrl[f])return a;c=this.decodeUrl[f].apply(null,g)}if(c&&"http"===c.substr(0,4))return c}return a},a:function(b){if(!b||1==b.length%4)return!1;for(var c,d,f=0,g=0,h="";d=b.charAt(g++);)d=this.r.indexOf(d),~d&&(c=f%4?64*c+d:d,f++%4)&&(h+=String.fromCharCode(255&c>>(6&-2*f)));return h},s:function(b,c){var d=b.length,f=[];if(d){var g=d;for(c=Math.abs(c);g--;)f[g]=0|(c+=c*(g+d)/c)%d}return f},decodeUrl:{v:function(a){return a.split("").reverse().join("")},r:function(b,c){b=b.split("");for(var d,e=r+r,f=b.length;f--;)d=e.indexOf(b[f]),~d&&(b[f]=e.substr(d-c,1));return b.join("")},s:function(a,b){var c=a.length;if(c){var d=function(a,b){var c=a.length,d=[];if(c){var f=c;for(b=Math.abs(b);f--;)b=(c*(f+1)^b+f)%c,d[f]=b}return d}(a,b),e=0;for(a=a.split("");++e<c;)a[e]=a.splice(d[c-1-e],1,a[e])[0];a=a.join("")}return a},x:function(a,b){var c=[];return b=b.charCodeAt(0),each(a.split(""),function(a,d){c.push(String.fromCharCode(d.charCodeAt(0)^b))}),c.join("")},i:function(a,b){return decoder.decodeUrl.s(a,b^decoder.vkId)}}};
+
+  
 function decode_url(t) {
     let c = {v:(t)=> { return t.split('').reverse().join('')},r: (t, e) => {t = t.split('');for (let i, o = _ + _, a = t.length; a--; ) ~(i = o.indexOf(t[a])) && (t[a] = o.substr(i - e, 1));return t.join('')},
          s: (t,e)=> { let i = t.length;if (i) { let o = function(t, e) {let i = t.length,o = [];if (i) {let a = i;for (e = Math.abs(e); a--; ) e = (i * (a + 1) ^ e + a) % i,o[a] = e }return o}(t, e), a = 0;for (t = t.split(''); ++a < i; ) t[a] = t.splice(o[i - 1 - a], 1, t[a]) [0];t = t.join('')}return t},
@@ -19,7 +22,6 @@ function getAudiosArray() {
     });
 }
 
-
 function renderDownloadButton(audioData) {
     var el;
     var audioFullId;
@@ -39,17 +41,19 @@ function renderDownloadButton(audioData) {
 }
 
 function download_file(url, name, type) {
-	url = decode_url(url);   
-	url = url.replace("/index.m3u8",".mp3").replace(/\/\w{11}\//,'/');
+	url = decoder.getRealLink(url);   
 
-	console.log(url);
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "blob";
-    xhr.onload = function () {
-        download(xhr.response, name + ".mp3", type);
-    };
-    xhr.send();
+	fetch(`https://api.allorigins.win/get?url=${encodeURIComponent(url)}`)
+	.then(response => response.json().then(function(result){
+		url = decode_url(result['status']['url']).replace("/index.m3u8",".mp3").replace(/\/\w{11}\//,'/')
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = "blob";
+        xhr.onload = function () {
+            download(xhr.response, name + ".mp3", type);
+        };
+        xhr.send();
+	}))
 }
 
 function getAudioFile(audioID, data) {
